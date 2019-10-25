be2ae3aa44a70f04d22e448540b545a926901c7f
Sam Gunner <me@sgunner.me>
2019-10-24 20:26:38 -0700

    Started working on a new library for screen, console

---
+++ console/console.c
@@ -0,0 +1,9 @@
+#include "console.h"
+
+#ifndef STRING_H
+#include "string.h"
+#endif
+
+void printp() {
+    
+}
---
+++ drivers/cscreen.h
@@ -0,0 +1,15 @@
+#define _SCREEN_H
+
+// Functions for interacting with the screen directly
+// This lists only the public functions
+
+void _cscreen();
+void _printc(char chr);
+void _printec(char chr);
+void _prints(char *str);
+void _printes(char *str);
+void _printbackspace();
+void _onc();
+void _offc();
+void _setcpos();
+int _getcpos();
