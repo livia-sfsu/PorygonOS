a5fab9333a36509d0802677e565a9bc21c6f1793
Think <going.to.any.lengths.work@gmail.com>
2019-11-02 09:02:11 -0700

    added memory.h stepping stone for paging

---
+++ memory/memory.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../cpu/types.h"
+/**
+ * Copy bytes between two memory locations
+ */
+void memcpy(void* source, void* dest, uint32_t num_bytes);
+
+/**
+ * Set the values from location to location + num_bytes to the given byte value
+ */
+void memset(void* location, uint8_t value, uint32_t num_bytes);
